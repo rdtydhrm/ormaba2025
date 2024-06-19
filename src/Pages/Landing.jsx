fd39864ae4d02dc4aa5bf5b922ab80650b5a53a4
kmdavidds <kmdavidds@gmail.com>
2024-06-20 02:28:22 +0700

    `feat:` Added ORMABA logo and after movie to landing page

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -2,13 +2,27 @@ import { Box, Button, Container, Typography } from "@mui/material";
 
 function Landing() {
     return (
-        <Container sx={{ height: '100vh' }}>
-            <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-                <Typography variant="h2" sx={{ textAlign: 'center', mb: 2, }}> ORMABA </Typography>
-                <Button href="/#/login" variant="outlined" size="medium" sx={{color: 'primary.main'}}>Login</Button>
-            </Box>
-        </Container>
-    )
+        <div>
+            <video autoPlay loop muted style={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%',
+                height: '100%',
+                objectFit: 'cover',
+                zIndex: -1,
+            }}>
+                <source src="/aftermovie3.mp4" type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
+            <Container sx={{ position: 'relative', height: '100vh', overflow: 'hidden', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', position: 'relative', zIndex: 1 }}>
+                    <Typography variant="h2" sx={{ textAlign: 'center', mb: 2 }}>O R M A B A</Typography>
+                    <Button href="/#/login" variant="outlined" size="medium" sx={{ color: 'primary.main' }}>Login</Button>
+                </Box>
+            </Container>
+        </div>
+    );
 }
 
-export default Landing
+export default Landing;
