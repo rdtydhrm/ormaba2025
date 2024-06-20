caa2663b5b01565df6f81464246662bc6dcb9137
kmdavidds <kmdavidds@gmail.com>
2024-06-20 23:57:17 +0700

    `feat:` Update login page with password visibility toggle

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,14 +1,22 @@
-import { Alert, Box, CircularProgress, Container, Fab, Snackbar, TextField, Typography } from "@mui/material";
+import { Alert, Box, CircularProgress, Container, Fab, FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput, Snackbar, TextField, Typography } from "@mui/material";
 import LockIcon from '@mui/icons-material/Lock';
 import LoginIcon from '@mui/icons-material/Login';
 
 import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
 import { useState } from "react";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
 
 function Login() {
     const [nim, setNim] = useState('');
     const [password, setPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
+
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+  
+    const handleMouseDownPassword = (event) => {
+      event.preventDefault();
+    };
 
     const mutation = useMutation({
         mutationFn: (loginDetails) => {
@@ -49,16 +57,34 @@ function Login() {
                     label='NIM'
                     value={nim}
                     onChange={(e) => setNim(e.target.value)} 
-                    sx={{my: 3}}
+                    sx={{my: 3, width: '25ch'}}
                 />
-                <TextField
+                <FormControl 
                     required 
                     label='Password' 
                     type="password"
                     value={password} 
                     onChange={(e) => setPassword(e.target.value)}  
-                    sx={{mb: 3}}
+                    sx={{mb: 3, width: '25ch'}}>
+                <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                <OutlinedInput
+                    id="outlined-adornment-password"
+                    type={showPassword ? 'text' : 'password'}
+                    endAdornment={
+                    <InputAdornment position="end">
+                        <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                        edge="end"
+                        >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                    </InputAdornment>
+                    }
+                    label="Password"
                 />
+                </FormControl>
                 <Typography variant="overline" sx={{color: 'primary.main', mb: 3}}>Gunakan Akun SIAM  <LockIcon fontSize="inherit"/></Typography>
                 {mutation.isPending ? (
                 <CircularProgress />
