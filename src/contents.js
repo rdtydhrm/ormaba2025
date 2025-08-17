8c1880d6fa470b0c0ac69334fb831f1ea886edc9
RadityaDharma <radityadharma6@gmail.com>
2025-08-17 08:04:40 +0700

    fix admin table

--- src/contents.js
+++ src/contents.js
@@ -1,4 +1,4 @@
-export const ormabaGroups = ["Sakhya", "Bimantara", "Cakrabhuwana", "Devendra", "Mahesvara", "Nishkala", "Osha", "Prabhu"]
+export const ormabaGroups = ["Sakhya", "Bandhana", "Kutumbha", "Sambandha", "Sangha", "Maitri", "Saumya", "Karuna"]
 
 export const whatIsORMABAshort = "ORMABA adalah kegiatan yang membentuk karakter mahasiswa baru dengan menanamkan nilai-nilai moral, spiritualitas, jiwa kepemimpinan, dan rasa kekeluargaan untuk menciptakan suasana positif dan produktif."
 
